b7a94809f7781fefd084c6e500318661925a9599
jamesscreen <jamesscreen@yandex.ru>
2018-10-05 16:35:47 +0300

    добавлены переменные и константы; запуск инициализации при загрузке страницы.

---
+++ script.js
@@ -0,0 +1,15 @@
+//задаем параметры игры
+var FIELD_SIZE_X = 20;//строки
+var FIELD_SIZE_Y = 20;//столбцы
+var SNAKE_SPEED = 200; // Интервал между перемещениями змейки
+var snake = []; // Сама змейка
+var direction = 'y+'; // Направление движения змейки
+var gameIsRunning = false; // Запущена ли игра
+var snake_timer; // Таймер змейки
+var food_timer; // Таймер для еды
+var score = 0; // Результат
+
+
+
+// выполняем инициализацию после загрузки страницы
+window.onload = init;
